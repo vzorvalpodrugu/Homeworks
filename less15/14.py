e28f212858a2d6b10116937f79e7243bd7651948
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-03-15 10:54:20 +0300

    Упорядочил директорию

--- less15/14.py
+++
@@ -1,9 +0,0 @@
-def print_user_info(**user_info):
-    print(user_info)
-    for key, value in user_info.items():
-        print(f"{key} = {value}")
-
-print_user_info(name="Иван", last_name="Морозов", age=25)
-
-user_info = {"name":"Катя", "last_name":"Блохова"}
-print_user_info(**user_info)
