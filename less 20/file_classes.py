20da434f01dba3f221dd3b24ea65aefbfa00de49
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-04-06 16:06:18 +0300

    Добавлены классы TxtFile и CsvFile для работы с соответствующими типами фалов.

--- less 20/file_classes.py
+++ less 20/file_classes.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-import json
+import json, csv
 
 class AbstractFile(ABC):
     """
@@ -21,7 +21,7 @@ class AbstractFile(ABC):
         pass
 
     @abstractmethod
-    def append(self):
+    def append(self, data):
         pass
 
     def __str__(self):
@@ -58,3 +58,69 @@ class JsonFile(AbstractFile):
         with open(self.filepath, 'a') as file:
             json.dump(data, file)
 
+class TxtFile(AbstractFile):
+    """
+    Class for TXT files.
+    """
+    def __init__(self, filepath: str):
+        super().__init__(filepath)
+
+    def read(self):
+        """
+        Read and return the content of the TXT file.
+        :return: Contents of the TXT file.
+        """
+        with open(self.filepath, 'r') as file:
+            return file.read()
+
+    def write(self, data):
+        """
+        Write the given data to the file.
+        :param data: Data to be written.
+        """
+        with open(self.filepath, 'w') as file:
+            file.write(data)
+
+    def append(self, data):
+        """
+        Append the given data to the file.
+        :param data: Data to be appended.
+
+        Args:
+            *data:
+        """
+        with open(self.filepath, 'a') as file:
+            file.write(data)
+
+class CvsFile(AbstractFile):
+    """
+    Class for CSV files.
+    """
+    def __init__(self, filepath: str):
+        super().__init__(filepath)
+
+    def read(self):
+        """
+        Read and return the content of the CSV file.
+        :return: List of rows in the CSV file.
+        """
+        with open(self.filepath, 'r') as file:
+            return csv.reader(file)
+
+    def write(self, data):
+        """
+        Write the given data to the file as a CSV file.
+        :param data: List of rows to be written.
+        """
+        with open(self.filepath, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+    def append(self, data):
+        """
+        Append the given data to the file as a CSV file.
+        :param data: List of rows to be appended.
+        """
+        with open(self.filepath, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
