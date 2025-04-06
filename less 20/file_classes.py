94ebe775ef111046f2b339936e6e719c440f1134
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-04-06 15:52:15 +0300

    1.Написан класс AbstractFile, который наследуется от ABC 2.Написан класс JsonFile, который наследуется от AbstractFile, для работы с Json файлами

---
+++ less 20/file_classes.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+import json
+
+class AbstractFile(ABC):
+    """
+    Abstract base class for files.
+    """
+    def __init__(self, filepath: str):
+        """
+        Initialize the file with its filepath.
+        :param filepath: Path to the file.
+        """
+        self.filepath = filepath
+
+    @abstractmethod
+    def read(self):
+       pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+    @abstractmethod
+    def append(self):
+        pass
+
+    def __str__(self):
+        return f"Class: {self.__class__.__name__} \nFile: {self.filepath}"
+
+class JsonFile(AbstractFile):
+    """
+    Class for JSON files.
+    """
+    def __init__(self, filepath: str):
+        super().__init__(filepath)
+
+    def read(self):
+        """
+        Read and return the content of the JSON file.
+        :return: JSON data loaded from the file.
+        """
+        with open(self.filepath, 'r') as file:
+            return json.load(file)
+
+    def write(self, data):
+        """
+        Write the given JSON data to the file.
+        :param data: JSON data to be written.
+        """
+        with open(self.filepath, 'w') as file:
+            json.dump(data, file)
+
+    def append(self, data):
+        """
+        Append the given JSON data to the file.
+        :param data: JSON data to be appended.
+        """
+        with open(self.filepath, 'a') as file:
+            json.dump(data, file)
+
