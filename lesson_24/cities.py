fe91bc0b6e0f5b8c47792ce5714dcb527621699c
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-04-16 14:35:03 +0300

    создан датакласс City для хранения данных о городах
    создан класс CitiesSerializer, который использует датакласс City для хранения информации о городах.
    
    в классе CitiesSerializer описан метод load_cities, который загружает данные о городах из json файла и создает объекты City.

--- lesson_24/cities.py
+++ lesson_24/cities.py
@@ -1,4 +1,5 @@
 import json
+from dataclasses import dataclass, field
 class JsonFile:
     """
     Класс для работы с json файлами
@@ -23,6 +24,51 @@ class JsonFile:
         with open(self.file_name, 'w') as file:
             json.dump(data, file)
 
+
+
+@dataclass
+class City:
+    """
+    Класс для хранения данных о городах
+    """
+    name: str
+    population: int
+    subject: str
+    district: str
+    latitude: float
+    longitude: float
+    is_used: bool = field(default=False)
+
+class CitiesSerializer:
+    """
+    Использует датакласс City для хранения информации о городах.
+    """
+    def __init__(self, json_file: JsonFile):
+        self.json_file = json_file
+        self.cities = []
+
+    def load_cities(self):
+        """
+        Загружает данные о городах из json файла и создает объекты City.
+        Returns:
+
+        """
+        cities_data = self.json_file.read_data()
+        for city_data in cities_data:
+            city = City(
+                name=city_data['name'],
+                population=city_data['population'],
+                subject=city_data['subject'],
+                district=city_data['district'],
+                latitude=city_data['latitude'],
+                longitude=city_data['longitude']
+            )
+            self.cities.append(city)
+
 # file = JsonFile('cities.json')
+# Serial = CitiesSerializer(file)
+# Serial.load_cities()
+# print(Serial.cities)
+
 # print(file.read_data())
 
